8c1743ebd0e23b004f7eb89da35d5dc4d9d18c4e
Anukul23 <141473676+Anukul23@users.noreply.github.com>
2025-01-09 20:41:47 +0530

    fetch-cards

---
+++ src/Countries.jsx
@@ -0,0 +1,37 @@
+
+
+import React, { useEffect, useState } from 'react';
+import "./Countries.css" ;
+
+const Countries = () => {
+const[countries , setcountries] = useState([]);
+
+useEffect(()=>{
+
+    const fetchCountries = async () => {
+        try{
+         const response = await fetch("https://xcountries-backend.azurewebsites.net/all") ;
+         const jasondata = await response.json();
+         setcountries(jasondata);
+        }
+        catch(error){
+          console.log("Error fetching data: ", error);
+        }
+    }
+   fetchCountries();
+} ,[])
+
+  return (
+    <div className='container'>
+
+      {countries.map((item)=>(
+       <div key={item.abbr}  className='card'>
+        <img src = {item.flag} alt={`Flage Of ${item.name}`} className='image'/>
+        <h2>{item.name}</h2>
+       </div>
+      ))}
+    </div>
+  )
+}
+
+export default Countries
